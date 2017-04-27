17cbc259338cb345663e5f096da6d339a2003ab3
A. Nelson <aaron.nelson805@gmail.com>
2017-04-27 15:00:29 -0500

    added features/swapped api for vod
    added weather alerts and switched votd api due to complications with the old api being unreliable.

--- bashwxplus0.1.py
+++ bashwxplus0.1.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python3
 
 import grequests, socket
-from colorama import Fore, Style
+from colorama import Fore, Back, Style
 from colr import Colr as C
 import os
 
 #build the query with the api keys given to us from OW
-api_id = '' ##insert your API key here to retrieve weather data
-URLS = ['http://api.wunderground.com/api/'+api_id+'/conditions/q/autoip.json',
-        'http://www.ourmanna.com/verses/api/get/?format=json',
-        'http://quotes.rest/qod.json',
-        'http://httpbin.org/ip']
+URLS = ['http://api.wunderground.com/api/705c1ebaea010d2e/conditions/q/autoip.json',
+        'http://api.wunderground.com/api/705c1ebaea010d2e/alerts/q/autoip.json',
+        'http://httpbin.org/ip',
+        'http://labs.bible.org/api/?passage=votd&type=json']
+
+alerts = {'HUR': 'Hurricane Local Statement', 
+          'TOR': 'Tornado Warning',
+          'TOW': 'Tornado Watch',
+          'WRN': 'Severe Thunderstorm Warning',
+          'SEW': 'Severe Thunderstorm Watch',
+          'WIN': 'Winter Weather Advisory',
+          'FLO': 'Flood Warning',
+          'WAT': 'Flood Watch/Statement',
+          'WND': 'High Wind Advisory',
+          'SVR': 'Severe Weather Statement',
+          'HEA': 'Heat Advisory',
+          'FOG': 'Dense Fog Advisory',
+          'FIR': 'Fire Weather Advisory',
+          'VOL': 'Volcanic Activity Statement',
+          'HWW': 'Hurricane Wind Warning',
+          'REC': 'Record Set',
+          'REP': 'Public Reports',
+          'PUB': 'Public Information Statement',
+          ' ' : 'None'}
 
 home_dir = os.environ['HOME']
 work_dir = home_dir + '/bashwx/'
 plus_file = work_dir + 'motdplus'
+alerts_file = work_dir + 'alerts'
 
 try:
     requests = (grequests.get(url) for url in URLS)
@@ -22,9 +42,9 @@ try:
     data = [response.json() for response in responses]
     
     current_wx = data[0]['current_observation']
-    vod = data[1]['verse']['details']
-    qod = data[2]['contents']['quotes'][0]
-    p_ip = data[3]['origin']
+    wx_alert = data[1]['alerts']
+    p_ip = data[2]['origin']
+    votd = data[3][0]
     
     #weather data
     loc = current_wx['observation_location']
@@ -34,25 +54,28 @@ try:
     temp_f = current_wx['temp_f'] #temp in F
     feels = current_wx['feelslike_f'] #feels like
     wind = current_wx['wind_string'] #wind data
+    gust = current_wx['wind_gust_mph'] #wind gust
     pressure = current_wx['pressure_mb'] #barometric pressure
     trend = current_wx['pressure_trend'] #pressure change (rising (+), falling(-))
     rain = current_wx['precip_today_in'] #daily precipitation
     time = current_wx['observation_time'] #observation time
     humid = current_wx['relative_humidity'] #realtive humidity
 
-    #verse of the day
-    verse = vod['text']
-    version = vod['version']
-    ref = vod['reference']
-
-    #quote of the day
-    quote = qod['quote']
-    author = qod['author']
-   
+    if len(wx_alert) > 0:
+         alert = str(Fore.WHITE + Back.RED + wx_alert['type'] + Fore.RESET + Back.RESET) #active weather alert/statement
+    else:
+         alert = (Fore.CYAN + Style.BRIGHT + 'No active weather alerts or statements' + Fore.RESET + Style.RESET_ALL)
+      
     #local ip
     loc_ip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     loc_ip.connect(('8.8.4.4', 0))
     locip = loc_ip.getsockname()[0] 
+
+    #votd
+    vod_book = votd['bookname']
+    vod_chap = votd['chapter']
+    vod_verse = votd['verse']
+    vod_text = votd['text']
     
     #motd 
     f = open('/etc/motd', 'r')
@@ -68,13 +91,14 @@ try:
         f.write(Style.BRIGHT  + 'Your ' + Fore.CYAN + 'Public IP ' + Fore.RESET + 'is [' + Fore.CYAN + p_ip + Fore.RESET + '] Your ' + Fore.CYAN + 'Local IP ' + Fore.RESET + 'is [' + Fore.CYAN + locip + Fore.RESET + ']\n\n' + Style.RESET_ALL)
         f.write(' Weather at [' + Fore.CYAN + Style.BRIGHT + local + Style.RESET_ALL + Fore.RESET + '] from station [' + Fore.CYAN + Style.BRIGHT + wx_station + Style.RESET_ALL + Fore.RESET + ']\n')
         f.write(' Conditions: [' + Fore.CYAN + Style.BRIGHT + weather + Style.RESET_ALL + Fore.RESET + ']\n')
-        f.write(' Winds: [' + Fore.CYAN + Style.BRIGHT + wind + Style.RESET_ALL + Fore.RESET + ']\n')
-        f.write(' Temp: [' + Fore.CYAN + Style.BRIGHT + str(temp_f) + Style.RESET_ALL + Fore.RESET + '] Feels like: [' + Fore.CYAN + Style.BRIGHT + str(feels) + Style.RESET_ALL + Fore.RESET + ']\n')
+        f.write(' Winds: [' + Fore.CYAN + Style.BRIGHT + wind + Style.RESET_ALL + Fore.RESET + '] gusting to [' + Fore.CYAN + Style.BRIGHT + str(gust) + 'MPH' + Fore.RESET + Style.RESET_ALL + ']\n')
+        f.write(' Temp: [' + Fore.CYAN + Style.BRIGHT + str(temp_f) + Style.RESET_ALL + Fore.RESET + '] feels like: [' + Fore.CYAN + Style.BRIGHT + str(feels) + Style.RESET_ALL + Fore.RESET + ']\n')
         f.write(' Humidity: [' + Fore.CYAN + Style.BRIGHT + humid + Style.RESET_ALL + Fore.RESET + ']\n')
         f.write(' Barometric Pressure: [' + Fore.CYAN + Style.BRIGHT + str(pressure) + ' mB' + ' (' + trend + ')' + Fore.RESET + Style.RESET_ALL + ']\n')
+        f.write(' Active alert: [' + alert + ']\n')
         f.write(' [' + Fore.CYAN + Style.BRIGHT + time + Fore.RESET + Style.RESET_ALL + ']\n\n')
-        f.write('Daily Bible Verse: ' + Fore.CYAN + Style.BRIGHT + verse + Fore.RESET + ' - ' + ref + Fore.RESET + Style.RESET_ALL +' [' + Fore.CYAN + Style.BRIGHT+ version + Style.RESET_ALL + ']\n\n')
-        f.write('Quote of the Day: ' + Fore.CYAN + Style.BRIGHT + quote + Fore.RESET + Style.RESET_ALL + ' - [' + Style.BRIGHT + Fore.CYAN + author + Fore.RESET + Style.RESET_ALL + ']\n')
+        f.write(' Verse of the Day: [' + Fore.CYAN + Style.BRIGHT + "\"" + vod_text + "\" - " + vod_book + ' ' + vod_chap + ":" + vod_verse + Fore.RESET + Style.RESET_ALL + ']\n')
+        
 
 except (AttributeError, NameError):
     raise
